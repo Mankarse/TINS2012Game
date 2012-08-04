29212fff105e43bafb25187a29f374b902f0050d
Evan Wallace <onlyone@senet.com.au>
2012-08-05 00:35:55 +1000

    Begun on game mechanic implementation

---
+++ src/obj/Dragon.cpp
@@ -0,0 +1,15 @@
+//
+//  Dragon.cpp
+//  TINSGame2012
+//
+//  Created by Evan Wallace on 4/08/12.
+//
+//
+
+#include "Dragon.h"
+    void Dragon::physicsStep(InputState const& input) {
+        currentSkin = &baby;
+    }
+    void Dragon::renderStep(Point2D screenPos) const {
+    
+    }
