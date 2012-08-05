e03dd2dca1841c0c50e9eb7f1af9c1ce4918ffc0
Evan Wallace <evan@Goanna.local>
2012-08-05 11:23:11 +1000

    Updated DragonStats

--- src/obj/DragonStats.h
+++ src/obj/DragonStats.h
@@ -1,18 +1,12 @@
-//
-//  DragonStats.h
-//  TINSGame2012
-//
-//  Created by Evan Wallace on 5/08/12.
-//
-//
-
 #ifndef TINSGame2012_DragonStats_h
 #define TINSGame2012_DragonStats_h
 
 struct DragonStats{
     double size;
     double wingspan;
-    double maxStam;
+    double stamina;
+    double fireStrength;
+    double fireCooldown;
 };
 
 #endif
