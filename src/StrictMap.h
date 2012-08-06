7d1d9b7a2a624ebdfade228c8f3b8f0332e50055
Evan Wallace <evan@Goanna.local>
2012-08-06 20:21:02 +1000

    Compressed resources

---
+++ src/StrictMap.h
@@ -0,0 +1,15 @@
+#ifndef TINSGame2012_StrictMap_h
+#define TINSGame2012_StrictMap_h
+#include <map>
+#include <cassert>
+template <typename K, typename V>
+struct StrictMap : std::map<K,V>{
+    typedef std::map<K,V> base_t;
+    V& operator[](K const& k) {
+        assert(base_t::find(k) != base_t::end());
+        return base_t::operator[](k);
+    }
+};
+
+
+#endif
